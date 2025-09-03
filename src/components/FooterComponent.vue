2e0a6095249bb9b777e65222798f47128e2df6b0
mariisbx <mariaaparecida.ifc.araquari@gmail.com>
2025-09-03 15:02:27 -0300

    FEAT: Conclusão do header #9

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,6 +3,17 @@
 </script>
 
 <template>
-
+    <footer class="px-22 bg-[#386641] text-[#FFFCF7] flex justify-between items-center">
+        <div>
+            <img src="/public/LogoFooter.png" alt="Logo">
+        </div>
+        <div>
+            <p>2025 &copy; IFComunidade</p>
+        </div>
+        <div>
+            <p class="font-bold flex items-center">Contato</p>
+            <p class="flex items-center gap-2"><span class="mdi mdi-email-outline text-2xl"></span> <span>ifcomunidade@gmail.com</span></p>
+        </div>
+    </footer>
 </template>
 
