b21f6b7236a2986c70de0e0db4dd72d6bf3a239e
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-09-04 10:07:14 -0300

    FEAT: criando api.js (com axios interceptor) #25

---
+++ src/plugins/api.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://localhost:8000/api',
+})
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  async function (error) {
+    const originalRequest = error.config
+    if (error.response && error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true
+      try {
+        const refreshToken = localStorage.getItem('refreshToken')
+        if (!refreshToken) {
+          throw new Error('Refresh token não encontrado')
+        }
+
+        const { data } = await axiosInstance.post('token/refresh/', {
+          refresh: refreshToken,
+        });
+
+        const newAccessToken = data.access
+        localStorage.setItem('token', newAccessToken)
+        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        delete axiosInstance.defaults.headers.common['Authorization']
+        window.location.href = '/Login';
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
