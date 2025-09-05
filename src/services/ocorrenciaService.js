e3978310937ae9ae7cbf0ef0eb09c5aa8cc090c8
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-09-05 15:09:08 -0300

    FEAT: criando OcorrenciaService e OcorrenciaStore [REFS: #35]

---
+++ src/services/ocorrenciaService.js
@@ -0,0 +1,63 @@
+import api from '@/axios/api'
+
+export default class OcorrenciaService {
+  async getAllOcorrencia() {
+    try {
+      const { data } = await api.get('/ocorrencias');
+      return data.results
+    }
+    catch(error) {
+      console.error('Erro ao buscar ocorrências:', error);
+      throw error;
+    }
+  }
+
+  async getOcorrenciaById(id) {
+    try {
+      const { data } = await api.get(`/ocorrencias/${id}`);
+      return data.results;
+    } catch (error) {
+      console.error(`Erro ao buscar ocorrência com ID ${id}:`, error);
+      throw error;
+    }
+  }
+
+  async addOcorrencia(NovaOcorrencia) {
+    try {
+      const { data } = await api.post('/ocorrencias', NovaOcorrencia);
+      return data;
+    } catch (error) {
+      console.error('Erro ao adicionar ocorrência:', error);
+      throw error;
+    }
+  }
+
+  async atualizarOcorrencia(id, OcorrenciaAtt) {
+    try {
+      const { data } = await api.put(`/ocorrencias/${id}`, OcorrenciaAtt);
+      return data;
+    } catch (error) {
+      console.error(`Erro ao atualizar ocorrência com ID ${id}:`, error);
+      throw error;
+    }
+  }
+
+  async attParcialmenteOcorrencia(id, OcorrenciaAtt) {
+    try {
+      const { data } = await api.patch(`/ocorrencias/${id}`, OcorrenciaAtt);
+      return data;
+    } catch (error) {
+      console.error(`Erro ao atualizar parcialmente ocorrência com ID ${id}:`, error);
+      throw error;
+    }
+  }
+
+  async deletarOcorrencia(id) {
+    try {
+      await api.delete(`/ocorrencias/${id}`);
+    } catch (error) {
+      console.error(`Erro ao deletar ocorrência com ID ${id}:`, error);
+      throw error;
+    }
+  }
+}
