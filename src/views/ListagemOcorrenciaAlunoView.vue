5a0698e14517e9389ecb35f8d35e9549ca4667ae
MCPavanello <pavanelloclaraa@gmail.com>
2025-09-09 16:52:52 -0300

    FEAT: Início da página de listagem de ocorrências #6

--- src/views/ListagemOcorrenciaAlunoView.vue
+++ src/views/ListagemOcorrenciaAlunoView.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-
+ <section class="px-22">
+    <div class="flex gap-6 mt-7 mb-10">
+    <img src="/Vector.png" alt="seta">
+    <h1 class="text-[#386641] text-2xl font-bold">Suas Ocorrências</h1>
+    </div>
+ </section>
 </template>
 
 <style scoped>
