4f13338e4b8c5a88e67120014718af8e7d228297
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2025-09-11 10:18:04 -0300

    FEAT: criar CategoriaService e CategoriaStore Fixes #38

---
+++ src/services/categoriaService.js
@@ -0,0 +1,26 @@
+import api from '@/plugins/api';
+
+class CategoriaService {
+  async getAllCategorias() {
+    try {
+      const { data } = await api.get('/categorias');
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error('Erro ao buscar categorias:', error);
+      throw error;
+    }
+  }
+
+  async getCategoriaByID(id) {
+    try {
+      const { data } = await api.get(`/categorias/${id}`);
+      return data.results;
+    } catch (error) {
+      console.error(`Erro ao buscar categoria com ID: ${id}:`, error);
+      throw error;
+    }
+  }
+}
+
+export default new CategoriaService();
