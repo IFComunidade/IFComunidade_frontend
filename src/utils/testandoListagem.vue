132a97a63c11551a3a51d8e8387ce601e0dda212
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2025-09-14 16:42:27 -0300

    TEST: Teste do funcionamento de obtenção de ocorrencia por ID Fixes #61

--- src/utils/testandoListagem.vue
+++ src/utils/testandoListagem.vue
@@ -1,43 +1,31 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import { useOcorrenciaStore } from '@/stores/ocorrenciaStore';
+import { useRouter } from 'vue-router';
 // import { usePostagemStore } from '@/stores/postagemStore';
 
 const ocorrenciaStore = useOcorrenciaStore();
-const ocorrenciaSelecionada = useOcorrenciaStore();
-const idOcorrencia = ref();
+const router = useRouter();
 
 onMounted(() => {
   ocorrenciaStore.getOcorrencias();
 });
-onMounted(() => {
-  ocorrenciaSelecionada.getOcorrenciaById(idOcorrencia.value);
-});
 
-const Boleano = ref(true);
-
-/*mudarBeSlavarID(o) {
-  Boleano.value = false;
-  idOcorrencia.value = o.id;
-}*/
+function abrirDetalhes(id) {
+  ocorrenciaStore.getOcorrenciaById(id);
+  router.push({ name: 'testandoListagemId', params: { id } });
+}
 
 </script>
 <template>
-  <div v-if="Boleano == true">
+  <div>
     <h1>Listagem de ocorrencias:</h1>
     <ul>
-      <li v-for="ocorrencia in ocorrenciaStore.ocorrencias" :key="ocorrencia.id">
-        <h2 @click="idOcorrencia.value = ocorrencia.id">{{ ocorrencia.titulo }}</h2>
+      <li v-for="ocorrencia in ocorrenciaStore.ocorrencias" :key="ocorrencia.id" class="border p-4 mb-4" @click="abrirDetalhes(ocorrencia.id)" style="cursor: pointer;">
+        <h2>{{ ocorrencia.titulo }}</h2>
         <p>{{ ocorrencia.texto }}</p>
         <p>{{ ocorrencia.status }}</p>
       </li>
     </ul>
-    <button @click="Boleano = false">Ver detalhes</button>
-  </div>
-  <div v-else>
-    <div v-for="item in ocorrenciaSelecionada.ocorrencias" :key="item.id">
-      <h2>{{ item.titulo }}</h2>
-    </div>
-    <button @click="Boleano = true">Voltar</button>
   </div>
 </template>
